634b0d9d153a5ca12f9c3fe4cd5e47eaa046b50c
ppthana <80138831+ppthana@users.noreply.github.com>
2024-01-14 22:13:25 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,15 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore/lite'
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getStorage, connectStorageEmulator } from 'firebase/storage'
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyD-l0_8-8pWb3c9cWoo4zDnYzsjA7TlXRY",
+    authDomain: "motopart-demo.firebaseapp.com",
+    databaseURL: "https://motopart-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "motopart-demo",
+    storageBucket: "motopart-demo.appspot.com",
+    messagingSenderId: "752871874265",
+    appId: "1:752871874265:web:628b220e0d4d3cfb1528c8"
+};
 
 const app = initializeApp(firebaseConfig);
 
